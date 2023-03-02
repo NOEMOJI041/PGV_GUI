2e3993d007eed359568ec2dbcac48349e4413138
NOEMOJI041 <aloksharma041@gmail.com>
2023-03-02 11:41:45 +0530

    2 test

--- img 4.py
+++ img 4.py
@@ -2,6 +2,7 @@ import socket
 from PIL import Image
 import io
 
+
 class Client:
     def __init__(self):
         self.ip = "192.168.2.2"
@@ -13,22 +14,36 @@ class Client:
         self.a = 0
         self.conn()
 
-
     def conn(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.ip, int(self.port)))
         # self.s.send(b'\xa8'b'\x57')
+
     def output(self):
 
         self.s.send(b'\x08'b'\xe0'b'\x00'b'\x00'b'\x00'b'\x00'b'\x00'b'\x00'b'\x00'b'\x00'b'\xe8')
         a = ''
-        self.redhead_output = self.s.recv(4).hex()
-        while True:
-            self.redhead_output = self.s.recv(4).hex()
-            print(self.redhead_output)
-            a = self.redhead_output + a
+        self.redhead_output = self.s.recv(2).hex()
+
+        for i in range(7000000):
+            print(i)
+            self.redhead_output = self.s.recv(2).hex()
+            # print(self.redhead_output)
+            with open("ok.txt", 'a') as file:
+                file.write(self.redhead_output)
+
             if self.redhead_output == "7fe0":
                 break
+
+        # for i in range(43000):
+        #     print(i)
+        #     self.redhead_output = self.s.recv(2).hex()
+        #     # print(self.redhead_output)
+        #     with open("ok.txt", 'a') as file:
+        #         file.write(self.redhead_output)
+        #
+        #     if self.redhead_output == "7fe0":
+        #         break
 
         print(len(a))
 
